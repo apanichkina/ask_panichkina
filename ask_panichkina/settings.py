ee5558add6cf5d5ce5e62e6357e4a20e64db37dc
anna <annjellyiu5@gmail.com>
2015-09-18 13:05:06 +0300

    login/logout/signup/avatar

--- ask_panichkina/settings.py
+++ ask_panichkina/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ask_panichkina',
-    'widget_tweaks',
+    #'widget_tweaks',
 )
 
 
@@ -92,6 +92,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+uploads = '/uploads/avatars/'
 
-uploads = '/uploads/'
+MEDIA_URL = '/uploads/avatars/'
+#MEDIA_ROOT = '/var/www/ask_dunin/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
